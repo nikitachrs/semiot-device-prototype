449070ed7ad90c486ecf849d4e235b75f36d69fb
Alexey Andreyev <aa13q@ya.ru>
2016-09-20 19:18:27 +0300

    docresource

--- libraries/semiot/minicoap/docresource/docresource.cpp
+++ libraries/semiot/minicoap/docresource/docresource.cpp
@@ -5,9 +5,35 @@
 DocResource::DocResource(MiniCoAP *coapServer):CoAPResource(coapServer)
 {
     resourcePath = {1,{"doc"}};
+}
 
-    updateAnswerJs();
+int DocResource::getMethod(const coap_packet_t *inpkt, coap_packet_t *outpkt)
+{
+    updateAnswer(); // FIXME: too frequent
+    return getServer()->coap_make_response(inpkt, outpkt,
+                                           (uint8_t*)answer.c_str(), strlen(answer.c_str()),
+                                           COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_APPLICATION_JSON);
+}
 
+void DocResource::updateAnswer()
+{
+    answer="";
+    doc_uri="";
+    doc_uri+=doc_protocol;
+    switch (WiFi.getMode()) {
+    case WIFI_STA:
+        doc_uri+=WiFi.localIP().toString();
+        break;
+    case WIFI_AP:
+        doc_uri+=WiFi.softAPIP().toString();
+        break;
+    default:
+        doc_uri+="localhost";
+        break;
+    }
+    doc_uri+=doc_path;
+    // FIXME: super-ugly:
+    StaticJsonBuffer<JS_BUF_LEN> jsBuffer;
     JsonObject& rootJson = jsBuffer.createObject();
     JsonObject& contextJsObj = rootJson.createNestedObject(context_prefix);
         contextJsObj[doc_prefix] = doc_uri;
@@ -24,35 +50,11 @@ DocResource::DocResource(MiniCoAP *coapServer):CoAPResource(coapServer)
             docTempDeviceJsObj[type_prefix] = ssnDevice_uri;
             docTempDeviceJsObj[semiotHasPrototype_uri] = tempDevicePrototype_uri;
             docTempDeviceJsObj[rdfsLabel_uri] = rdfsLabelLiteral;
-            docTempDeviceJsObj[dctermsIndentifier_uri] = sha1(WiFi.macAddress()).c_str();
+            docTempDeviceJsObj[dctermsIndentifier_uri] = sha1(WiFi.macAddress());
         JsonObject& docLatestProcessOutputJsObj = hydraSupportedClassJsObj.createNestedObject();
             docLatestProcessOutputJsObj[id_prefix] = docLatestProcessOutput_uri;
             docLatestProcessOutputJsObj[type_prefix] = hydraLink_uri;
             docLatestProcessOutputJsObj[rdfsDomain_uri] = semiotProcess_uri;
             docLatestProcessOutputJsObj[rdfsRange_uri] = semiotProcessOutput_uri;
     rootJson.printTo(answer);
-}
-
-int DocResource::getMethod(const coap_packet_t *inpkt, coap_packet_t *outpkt)
-{
-    updateAnswerJs(); // FIXME: too frequent
-    return getServer()->coap_make_response(inpkt, outpkt, (uint8_t*)answer.c_str(), strlen(answer.c_str()), COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_APPLICATION_JSON);
-}
-
-void DocResource::updateAnswerJs() // FIXME: not saving to json
-{
-    memset(doc_uri,0,DOC_URI_MAXLEN);
-    strcpy(doc_uri,doc_protocol);
-    switch (WiFi.getMode()) {
-    case WIFI_STA:
-        strcat(doc_uri,WiFi.localIP().toString().c_str());
-        break;
-    case WIFI_AP:
-        strcat(doc_uri,WiFi.softAPIP().toString().c_str());
-        break;
-    default:
-        strcat(doc_uri,"localhost");
-        break;
-    }
-    strcat(doc_uri,doc_path);
 }
--- libraries/semiot/minicoap/docresource/docresource.h
+++ libraries/semiot/minicoap/docresource/docresource.h
@@ -4,8 +4,7 @@
 #include <string.h>
 #include "coapresource.h"
 #include <ArduinoJson.h>
-#define ANSWER_JS_MAXLEN 1000
-#define DOC_URI_MAXLEN 60
+#define JS_BUF_LEN 1500
 
 class DocResource : public CoAPResource
 {
@@ -13,58 +12,55 @@ public:
     DocResource(MiniCoAP* coapServer);
     int getMethod(const coap_packet_t *inpkt, coap_packet_t *outpkt);
 private:
-    StaticJsonBuffer<ANSWER_JS_MAXLEN> jsBuffer;
     String answer;
-    // JsonObject& rootJson;
-    // JsonObject& contextJsObj;
 
-    void updateAnswerJs();
+    void updateAnswer();
 
-    const char *rdfsLabelLiteral = "New Device";
+    String rdfsLabelLiteral = "New Device";
 
-    const char *context_prefix = "@context";
-    const char *id_prefix = "@id";
-    const char *type_prefix = "@type";
+    String context_prefix = "@context";
+    String id_prefix = "@id";
+    String type_prefix = "@type";
 
-    const char *doc_prefix = "doc";
-    const char *doc_protocol = "coap://";
-    const char *doc_path = "/doc#";
-    char doc_uri[DOC_URI_MAXLEN];
-    const char *docApiDocumentation_uri = "doc:ApiDocumentation";
-    const char *docTempDevice_uri = "doc:TempDevice";
-    const char *docLatestProcessOutput_uri = "doc:latestProcessOutput";
+    String doc_prefix = "doc";
+    String doc_protocol = "coap://";
+    String doc_path = "/doc#";
+    String doc_uri;
+    String docApiDocumentation_uri = "doc:ApiDocumentation";
+    String docTempDevice_uri = "doc:TempDevice";
+    String docLatestProcessOutput_uri = "doc:latestProcessOutput";
 
-    const char *ssn_prefix = "ssn";
-    const char *ssn_uri = "http://purl.oclc.org/NET/ssnx/ssn#";
-    const char *ssnDevice_uri = "ssn:Device";
+    String ssn_prefix = "ssn";
+    String ssn_uri = "http://purl.oclc.org/NET/ssnx/ssn#";
+    String ssnDevice_uri = "ssn:Device";
 
-    const char *rdfs_prefix = "rdfs";
-    const char *rdfs_uri = "http://www.w3.org/2000/01/rdf-schema#";
-    const char *rdfsLabel_uri = "rdfs:label";
-    const char *rdfsDomain_uri = "rdfs:domain";
-    const char *rdfsRange_uri = "rdfs:range";
+    String rdfs_prefix = "rdfs";
+    String rdfs_uri = "http://www.w3.org/2000/01/rdf-schema#";
+    String rdfsLabel_uri = "rdfs:label";
+    String rdfsDomain_uri = "rdfs:domain";
+    String rdfsRange_uri = "rdfs:range";
 
-    const char *dcterms_prefix = "dcterms";
-    const char *dcterms_uri = "http://purl.org/dc/terms/";
-    const char *dctermsIndentifier_uri = "dcterms:identifier";
+    String dcterms_prefix = "dcterms";
+    String dcterms_uri = "http://purl.org/dc/terms/";
+    String dctermsIndentifier_uri = "dcterms:identifier";
 
-    const char *hydra_prefix = "hydra";
-    const char *hydra_uri = "http://www.w3.org/ns/hydra/core#";
-    const char *hydraApiDocumentation_uri = "hydra:ApiDocumentation";
-    const char *hydraSupportedClass_uri = "hydra:supportedClass";
-    const char *hydraLink_uri = "hydra:Link";
+    String hydra_prefix = "hydra";
+    String hydra_uri = "http://www.w3.org/ns/hydra/core#";
+    String hydraApiDocumentation_uri = "hydra:ApiDocumentation";
+    String hydraSupportedClass_uri = "hydra:supportedClass";
+    String hydraLink_uri = "hydra:Link";
 
 
-    const char *qudt_prefix = "qudt";
-    const char *qudt_uri = "http://qudt.org/schema/qudt#";
+    String qudt_prefix = "qudt";
+    String qudt_uri = "http://qudt.org/schema/qudt#";
 
-    const char *semiot_prefix = "semiot";
-    const char *semiot_uri = "https://w3id.org/semiot/ontologies/semiot#";
-    const char *semiotHasPrototype_uri = "semiot:hasPrototype";
-    const char *semiotProcess_uri = "semiot:Process";
-    const char *semiotProcessOutput_uri = "semiot:ProcessOutput";
+    String semiot_prefix = "semiot";
+    String semiot_uri = "https://w3id.org/semiot/ontologies/semiot#";
+    String semiotHasPrototype_uri = "semiot:hasPrototype";
+    String semiotProcess_uri = "semiot:Process";
+    String semiotProcessOutput_uri = "semiot:ProcessOutput";
 
-    const char *tempDevicePrototype_uri = "http://example.com/prototypes/TempDevice";
+    String tempDevicePrototype_uri = "http://example.com/prototypes/TempDevice";
 
 };
 
